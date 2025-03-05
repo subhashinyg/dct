e54e8b9c52d7e86cbc328ccb40eb447d7e07ffd6
sema-dev-team <devteam@semaphoremobile.com>
2025-03-05 21:59:48 +0530

    build

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import auth_route from './auth/auth-routes'
 import user_route from './user/user-routes'
 
 const AppEndPoints = (app:Application)=>{
-    app.get('/',(req,res)=>res.send('Welcome to Staff Blast v1.1.8.')) 
+    app.get('/',(req,res)=>res.send('Welcome To Dreams Chartitable Trust v.0')) 
     app.use('/admin',admin_route)
     app.use('/auth',auth_route)
     app.use('/user',user_route)
