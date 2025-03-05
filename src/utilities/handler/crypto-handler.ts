3d6ec052415e06b4b5269b2a5d451671ea335270
sema-dev-team <devteam@semaphoremobile.com>
2025-03-05 23:29:59 +0530

    fix

--- src/utilities/handler/crypto-handler.ts
+++ src/utilities/handler/crypto-handler.ts
@@ -1,11 +1,11 @@
-import CryptoJs from 'crypto-js'
+
 export class CryptoHandler{
 
-    CryptoEncrypt=(value:string,key:string):string=>{
-        return CryptoJs.AES.encrypt(value.toString(),key).toString()
-    }
+    // CryptoEncrypt=(value:string,key:string):string=>{
+    //     return CryptoJs.AES.encrypt(value.toString(),key).toString()
+    // }
 
-    CryptoDecrypt=(value:string,key:string):any=>{
-        return CryptoJs.AES.decrypt(value,key).toString(CryptoJs.enc.Utf8)
-    }
+    // CryptoDecrypt=(value:string,key:string):any=>{
+    //     return CryptoJs.AES.decrypt(value,key).toString(CryptoJs.enc.Utf8)
+    // }
 }
--- src/utilities/service/settings-service.ts
+++ src/utilities/service/settings-service.ts
@@ -1,5 +1,4 @@
 import { INotification } from "../../interface/common-interface"
-import { NotificationModel } from "../../models/notification/notification-model"
 import { PageNumberSanitizer, objectSanitizer } from "../handler/validation-handler"
 
 
